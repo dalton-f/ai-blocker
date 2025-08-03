efb3da53b7884f6657552256226bca1f851d9b32
Dalton <daltonfiggdavies@gmail.com>
2025-08-03 09:27:30 +0100

    Update extenstion to use MutationObserver so it always works

--- preventOverview.js
+++ preventOverview.js
@@ -1,6 +1,13 @@
-const aiOverview = document.getElementById("llm-snippet");
+const observer = new MutationObserver(() => {
+  const aiOverview = document.getElementById("llm-snippet");
+  const aiSideOverview = document.getElementById("llm-side-snippet");
 
-if (aiOverview) {
-  aiOverview.style.opacity = 0;
-  aiOverview.style.position = "absolute";
-}
+  if (aiOverview) aiOverview.style.display = "none";
+
+  if (aiSideOverview) aiSideOverview.style.display = "none";
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+});
