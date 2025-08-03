9bce6583a5f8c87d7b6a925589459fe96a44beb1
Dalton <daltonfiggdavies@gmail.com>
2025-08-03 10:17:30 +0100

    Update to work on google

--- preventOverview.js
+++ preventOverview.js
@@ -1,4 +1,14 @@
+const patterns = [/ai overview/i];
+
 const observer = new MutationObserver(() => {
+  // Handles Google
+  const hiddenAiLabel = [...document.querySelectorAll("h1, h2")].find((e) =>
+    patterns.some((pattern) => pattern.test(e.innerText))
+  );
+
+  if (hiddenAiLabel) hiddenAiLabel.parentNode.style.display = "none";
+
+  // Handles Brave Browser
   const aiOverview = document.getElementById("llm-snippet");
   const aiSideOverview = document.getElementById("llm-side-snippet");
 
