1bfbc8448ea399b20cdada8354d9f26d742f0cf2
Dalton <daltonfiggdavies@gmail.com>
2025-08-06 08:51:02 +0100

    Hide new AI mode tab

--- preventOverview.js
+++ preventOverview.js
@@ -1,7 +1,8 @@
 let hiddenAiLabel = null;
+let aiModeTab = null;
 
 const observer = new MutationObserver(() => {
-  // Handles Google
+  // Handles Google LLM snippets
   const headings = document.querySelectorAll("h1, h2");
 
   // Search for the hidden AI label to find the correct container to hide
@@ -9,20 +10,38 @@ const observer = new MutationObserver(() => {
     if (heading.innerText === "AI overview") hiddenAiLabel = heading;
   }
 
-  if (hiddenAiLabel) hiddenAiLabel.parentNode.style.display = "none";
+  if (hiddenAiLabel) {
+    // Find the upper level div which acts as the parent container and hide that
+    // This ensures the padding on the page is unaffected
+    const container = hiddenAiLabel.parentNode.parentNode.parentNode;
+    container.style.display = "none";
+  }
 
-  // Hides sponsored sites if an ad-blocker doesn't already
-  const sponsoredSites = document.getElementById("tads");
-
-  if (sponsoredSites) sponsoredSites.style.display = "none";
-
-  // Handles Brave Browser
+  // Handles Brave Browser LLM snippets
   const aiOverview = document.getElementById("llm-snippet");
   const aiSideOverview = document.getElementById("llm-side-snippet");
 
   if (aiOverview) aiOverview.style.display = "none";
 
   if (aiSideOverview) aiSideOverview.style.display = "none";
+
+  // Hides sponsored sites if an ad-blocker doesn't already
+  const sponsoredSites = document.getElementById("tads");
+
+  if (sponsoredSites) sponsoredSites.style.display = "none";
+
+  // Hide AI mode tab from the browser
+  const spans = document.querySelectorAll("span");
+
+  for (const span of spans) {
+    if (span.innerText === "AI Mode") aiModeTab = span;
+  }
+
+  if (aiModeTab) {
+    // Find the parent anchor tag
+    const anchor = aiModeTab.closest("a");
+    anchor.style.display = "none";
+  }
 });
 
 observer.observe(document, {
